55f8ff71ddc97b07177ec89857dfd24f00fdf39e
praveenpatil59 <praveenpatil59@yahoo.in>
2020-08-21 22:49:41 +0530

    changes comited to subranch

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -2,6 +2,11 @@
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println("added from sub branch");
 		System.out.println("Hello in master branch");
+		if(true)
+		{
+			System.out.println("added this in sub branch");
+		}
 	}
 }
