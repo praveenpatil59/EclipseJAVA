a9b9ccfc60f1f8a716a0146aaf272ae27a9704ee
praveenpatil59 <praveenpatil59@yahoo.in>
2020-08-21 23:39:41 +0530

    master1

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -2,23 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
-		int a=6;
-		for (int i = 0; i < 5; i++) {
-			System.out.println("inside for loop");
-			
-		}
-		while(a==5)
-		{
-			
-		}
-		if(true)
-		{
-			System.out.println("added this in sub branch");
-		}
-		int c=10;
-		while(c==10)
-		{
-			
-		}
+		
+		System.out.println("in master branch");
 	}
+	
 }
