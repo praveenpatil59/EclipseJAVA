45ecf6ac33dd2b42527a90371d37592238023900
praveenpatil59 <praveenpatil59@yahoo.in>
2020-08-21 23:24:40 +0530

    master chnag1

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -2,8 +2,13 @@
 public class Test {
 
 	public static void main(String[] args) {
+		int a=6;
 		for (int i = 0; i < 5; i++) {
 			System.out.println("inside for loop");
+			
+		}
+		while(a==5)
+		{
 			
 		}
 		if(true)
