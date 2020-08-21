397254604c8a092da315d000bf55e348e5f3e629
praveenpatil59 <praveenpatil59@yahoo.in>
2020-08-21 23:03:41 +0530

    in sub branc2

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -2,8 +2,10 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("added from sub branch");
-		System.out.println("Hello in master branch");
+		if (true||false)
+		{
+			System.out.println("newly added for merge conflict to get");
+		}
 		if(true)
 		{
 			System.out.println("added this in sub branch");
