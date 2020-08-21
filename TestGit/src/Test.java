6a7be29fd79ba8df8d4aeb33fcdeb0172501393f
praveenpatil59 <praveenpatil59@yahoo.in>
2020-08-21 22:33:55 +0530

    chaned in master

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -2,9 +2,6 @@
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
-		System.out.println("Hello in master commited not pushed nd i m doing changs in sub branch");
-		
-		System.out.println("Hello world from sub branch");
+		System.out.println("Hello in master branch");
 	}
 }
