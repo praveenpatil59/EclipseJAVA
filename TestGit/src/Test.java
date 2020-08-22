a80883d2a8114aa3a9351edd53ef09bc1e8734d5
praveenpatil59 <praveenpatil59@yahoo.in>
2020-08-22 11:47:51 +0530

    mastercmiit

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		if(true)
 		{
-			System.out.println("inside if of master");
+			System.out.println("inside if of master after checkout of sub5 branch");
 		}
 
 }
