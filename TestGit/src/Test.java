4015f27d3bd938137600eb50c9c40f03b305c5fe
praveenpatil59 <praveenpatil59@yahoo.in>
2020-08-22 12:17:57 +0530

    mstr

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		if(true)
 		{
-			System.out.println("inside if of master branch");
+			System.out.println("inside if of master branch chagning after subbranch checked out");
 		}
 
 }
